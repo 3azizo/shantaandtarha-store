ea944a670c4889ef2e1106350fa97b7c0a08d0f9
3azizo <mohamedreda7494591@gmail.com>
2025-01-01 21:48:17 +0200

    add the namra tech footer link to my website

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,12 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-3'>
-            <p>Shanta &amp; Tarha &copy; {currentYear}</p>
+          <p>
+          Shanta &amp; Tarha &copy; {currentYear} Powered by{' '}
+          <a href="https://www.namratech.com" target="_blank" rel="noopener noreferrer">
+            Namra Tech
+          </a>
+        </p>
           </Col>
         </Row>
       </Container>
